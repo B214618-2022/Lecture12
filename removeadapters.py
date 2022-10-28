dec38662ea107e9b67d6eb7adef978531631a221
Michael Beavitt <s1653324@bioinfmsc8.bio.ed.ac.uk>
2022-10-28 12:58:58 +0100

    remove adapters script complete

--- removeadapters.py
+++ removeadapters.py
@@ -2,19 +2,22 @@
 
 count=0
 my_file = open('./files/input.txt')
+new_file = open('trimmed.txt', 'w')
 
 for eachline in my_file :
     count+=1
     adapterfree = eachline[14:]
     len_af = len(adapterfree)
     print(f'The length of sequence  {count}  is  {len_af}')
-    
+    new_file.write(adapterfree + '\n')
+
+new_file.close()
 #    print('We are processing line {} of the file'.format(count))
 #    print(f'There are {line_length} characters on the line')
 
 
 #for colour_item in somecolours4 :
-#    myf = open(colour_item + '.txt', 'w')
+#    myfile = open(colour_item + '.txt', 'w')
 #    myfile.write(str(len(colour_item)) + '\n')
 #    myfile.close()
 #    f'{colour_item[0:3].upper()}'
