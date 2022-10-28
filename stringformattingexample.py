a142c376528cad3540f5eb7971dd25c5bf65a3e5
Michael Beavitt <s1653324@bioinfmsc8.bio.ed.ac.uk>
2022-10-28 11:45:08 +0100

    Doing some string formatting exercises

---
+++ stringformattingexample.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+somecolours3 = ['yellow','purple','black','pink','yellow','blue','red','green']
+
+count = 0
+for colour_item in somecolours3:
+    count+=1
+    print(f"Colour {count} is {colour_item}")
+
+
