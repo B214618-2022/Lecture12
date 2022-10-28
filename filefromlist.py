aaaa3c1a2e51b30d0ff79bfa38bcb2b780eb5a07
Michael Beavitt <s1653324@bioinfmsc8.bio.ed.ac.uk>
2022-10-28 16:59:25 +0100

    adding some comments

--- filefromlist.py
+++ filefromlist.py
@@ -1,9 +1,12 @@
 #!/bin/python3
 
+#Setting variables
 somecolours4 = ['blue','red','green']
 
+
 for colour_item in somecolours4:
-    myfile = open(colour_item + '.txt', 'w')
-    myfile.write(str(len(colour_item)) + '\n')
-    myfile.close()
-    print(f'{colour_item[0:3].upper()}')
+    myfile = open(colour_item + '.txt', 'w') #creating a .txt file and naming it the correct colour
+    myfile.write(str(len(colour_item)) + '\n') #writing the length of the string to the file
+    myfile.close() #closing the file
+    print(f'{colour_item[0:3].upper()}') #printing the first three letters of the colour in uppercase
+
