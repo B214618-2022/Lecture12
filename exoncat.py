21d9112490ee3e3a7e81a144656060c89b328360
Michael Beavitt <s1653324@bioinfmsc8.bio.ed.ac.uk>
2022-11-01 16:19:11 +0000

    starting from scratch

--- exoncat.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python3
-
-#Setting variables
-DNA_source = open('./files/genomic_dna2.txt')
-concatenated_exons = open('catexons.txt', 'w')
-exon_coords = open('./files/exons.txt')
-
-for line in exon_coords:
-    print(line)
